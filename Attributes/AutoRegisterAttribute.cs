739ad7b3c0c220dd3cec0e1872f3a883e4815588
hcj <hcj@digital-tec.cn>
2020-11-16 18:05:26 +0800

    优化

---
+++ Attributes/AutoRegisterAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace Easy.Common.NetCore.Attributes
+{
+    /// <summary>
+    /// 标记为自动IOC注册
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class AutoRegisterAttribute : Attribute
+    {
+    }
+}
---
+++ IoC/Autofac/ContainerBuilderExt.cs
@@ -0,0 +1,51 @@
+﻿using Autofac;
+using Easy.Common.NetCore.Attributes;
+using Easy.Common.NetCore.Extentions;
+using Easy.Common.NetCore.Repository;
+using System;
+using System.Reflection;
+
+namespace Easy.Common.NetCore.IoC.Autofac
+{
+    public static class ContainerBuilderExt
+    {
+        public static void RegisterRepoAndSvc(this ContainerBuilder builder, DataBaseType dataBaseType, Assembly repoAssembly, Assembly unitSvcAssembly, Assembly integrateSvcAssembly)
+        {
+            if (!dataBaseType.IsInDefined()) throw new Exception($"数据库类型dataBaseType：{dataBaseType}不合法");
+            if (repoAssembly == null) throw new Exception($"repoAssembly程序集不能为空");
+            if (unitSvcAssembly == null) throw new Exception($"unitSvcAssembly程序集不能为空");
+            if (integrateSvcAssembly == null) throw new Exception($"integrateSvcAssembly程序集不能为空");
+
+            //注册范型仓储
+            if (dataBaseType == DataBaseType.SqlServer)
+            {
+                builder.RegisterGeneric(typeof(SqlServerRepository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
+            }
+            else if (dataBaseType == DataBaseType.PostgreSQL)
+            {
+                builder.RegisterGeneric(typeof(PgSqlRepository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
+            }
+
+            var allTypes = AppDomain.CurrentDomain.GetAllTypes();
+
+            foreach (Type type in allTypes)
+            {
+                //是约定自动注册
+                bool isPromissoryRegisterType = type.GetCustomAttribute<AutoRegisterAttribute>() != null;
+
+                //约定是一个类，且是约定自动注册
+                if (type.IsClass && isPromissoryRegisterType)
+                {
+                    string typeName = type.Name;
+                    string interfaceName = $"I{typeName}";
+
+                    Type interfaceType = type.GetInterface(interfaceName);
+
+                    if (interfaceType == null) throw new Exception($"按约定类型{typeName}必须继承接口{interfaceName}");
+
+                    builder.RegisterType(type).As(interfaceType).SingleInstance();
+                }
+            }
+        }
+    }
+}
