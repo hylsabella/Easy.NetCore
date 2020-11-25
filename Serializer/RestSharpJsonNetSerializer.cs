f28db2b6839d6d01f8792cca858c1fbdc035beda
hcj <hcj@digital-tec.cn>
2020-11-25 14:22:53 +0800

    11

---
+++ Serializer/RestSharpJsonNetSerializer.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using RestSharp.Serializers;
+
+namespace Easy.Common.NetCore.Serializer
+{
+    public class RestSharpJsonNetSerializer : ISerializer
+    {
+        public string ContentType { get; set; } = "application/json";
+
+        public string Serialize(object obj)
+        {
+            var serializerSettings = new JsonSerializerSettings
+            {
+                DateFormatString = "yyyy-MM-dd HH:mm:ss",
+            };
+
+            return JsonConvert.SerializeObject(obj, serializerSettings);
+        }
+    }
+}
