636641036b8a90b0cacd3290b11dc5a3fee49a1e
hcj <hcj@digital-tec.cn>
2021-03-05 16:11:06 +0800

    11

---
+++ Extentions/DictionaryExt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Easy.Common.NetCore.Extentions
+{
+    public static class DictionaryExt
+    {
+        public static string GetUrlParams(this Dictionary<string, string> dict)
+        {
+            if (dict == null || !dict.Any())
+            {
+                return string.Empty;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in dict)
+            {
+                sb.Append($"{item.Key}={item.Value}&");
+            }
+
+            return sb.ToString().TrimEnd('&');
+        }
+    }
+}
