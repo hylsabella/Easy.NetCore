bd0ce90a74d749277a00ea9478b1f5701ba384a2
hcj <hcj@qq.com>
2021-04-15 10:44:00 +0800

    11

--- Helpers/CallHelper.cs
+++ Helpers/CallHelper.cs
@@ -14,53 +14,20 @@ namespace Easy.Common.NetCore.Helpers
         /// <param name="reTryDelay">重试间隔</param>
         /// <param name="remark">备注</param>
         /// <returns>true：重试成功；false：重试失败</returns>
-        public static bool RrTryRun(uint reTryCount, Func<bool> reTryAction, TimeSpan? reTryDelay = null, string remark = "")
+        public static bool ReTryRun(uint reTryCount, Func<bool> reTryAction, TimeSpan? reTryDelay = null, string remark = "")
         {
-            if (reTryCount <= 0) throw new FException("reTryCount至少重试1次");
-            if (reTryAction == null) throw new FException("action不能为空");
-
-            bool 是匿名方法 = reTryAction.GetType().Name.Contains("AnonymousType");
-            string methodName = 是匿名方法 ? "匿名方法" : reTryAction.Method.Name;
-
-            for (uint i = 1; i <= reTryCount; i++)
+            var reTryRunResult = ReTryRun(reTryCount, () =>
             {
-                if (i != 1)
+                bool isReTrySuccess = reTryAction();
+
+                return new ReTryRunResult<bool>
                 {
-                    LogHelper.Trace($"开始重试：{methodName} {remark}");
-                }
+                    IsReTrySuccess = isReTrySuccess,
+                    ActionResult = isReTrySuccess,
+                };
+            }, reTryDelay, remark);
 
-                try
-                {
-                    //如果执行抛出异常，那么重试
-                    bool 重试成功 = reTryAction();
-
-                    if (!重试成功)
-                    {
-                        bool 非最后一次 = i != reTryCount;
-                        if (非最后一次 && reTryDelay.HasValue)
-                        {
-                            Task.Delay(reTryDelay.Value).Wait();
-                        }
-
-                        continue;
-                    }
-
-                    if (i != 1)
-                    {
-                        LogHelper.Trace($"重试{i}次成功：{methodName} {remark}");
-                    }
-
-                    return true;
-                }
-                catch (Exception ex)
-                {
-                    LogHelper.Error(ex, $"{ex.Message} {remark}", "RrTryRun");
-                }
-            }
-
-            LogHelper.Trace($"重试失败：{methodName} {remark}");
-
-            return false;
+            return reTryRunResult.IsReTrySuccess;
         }
 
         /// <summary>
@@ -71,7 +38,7 @@ namespace Easy.Common.NetCore.Helpers
         /// <param name="reTryDelay">重试间隔</param>
         /// <param name="remark">备注</param>
         /// <returns>true：重试成功；false：重试失败</returns>
-        public static RrTryRunResult<T> RrTryRun<T>(uint reTryCount, Func<RrTryRunResult<T>> reTryAction, TimeSpan? reTryDelay = null, string remark = "")
+        public static ReTryRunResult<T> ReTryRun<T>(uint reTryCount, Func<ReTryRunResult<T>> reTryAction, TimeSpan? reTryDelay = null, string remark = "")
         {
             if (reTryCount <= 0) throw new FException("reTryCount至少重试1次");
             if (reTryAction == null) throw new FException("action不能为空");
@@ -89,9 +56,9 @@ namespace Easy.Common.NetCore.Helpers
                 try
                 {
                     //如果执行抛出异常，那么重试
-                    var RrTryRunResult = reTryAction();
+                    var reTryRunResult = reTryAction();
 
-                    if (!RrTryRunResult.IsRrTrySuccess)
+                    if (!reTryRunResult.IsReTrySuccess)
                     {
                         bool 非最后一次 = i != reTryCount;
                         if (非最后一次 && reTryDelay.HasValue)
@@ -107,23 +74,23 @@ namespace Easy.Common.NetCore.Helpers
                         LogHelper.Trace($"重试{i}次成功：{methodName} {remark}");
                     }
 
-                    return RrTryRunResult;
+                    return reTryRunResult;
                 }
                 catch (Exception ex)
                 {
-                    LogHelper.Error(ex, $"{ex.Message} {remark}", "RrTryRun");
+                    LogHelper.Error(ex, $"{ex.Message} {remark}", "ReTryRun");
                 }
             }
 
             LogHelper.Trace($"重试失败：{methodName} {remark}");
 
-            return new RrTryRunResult<T> { IsRrTrySuccess = false };
+            return new ReTryRunResult<T> { IsReTrySuccess = false };
         }
     }
 
-    public class RrTryRunResult<T>
+    public class ReTryRunResult<T>
     {
-        public bool IsRrTrySuccess { get; set; }
+        public bool IsReTrySuccess { get; set; }
 
         public T ActionResult { get; set; }
     }
