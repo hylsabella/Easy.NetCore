c439cdc71a528fe46d1439c3f1665fc69cd553b5
hcj <hcj@qq.com>
2021-05-28 10:47:32 +0800

    111

---
+++ Ext/ObjectExt.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace Easy.Common.NetCore
+{
+    public static class ObjectExt
+    {
+        public static string ToJsonStr(this object value)
+        {
+            if (value == null) return null;
+
+            return JsonConvert.SerializeObject(value);
+        }
+    }
+}
